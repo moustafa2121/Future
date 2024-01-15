fa36fcdb30197e0502cfdb1976e674896ccf084d
moustafa2121 <moustafa2121@gmail.com>
2024-01-15 22:28:07 +0400

    all done

--- TheApp/static/mainJS.js
+++ TheApp/static/mainJS.js
@@ -1,12 +1,31 @@
 window.onscroll = () => { 
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementsByTagName("header")[0].style.padding = "2px 0px";
-        document.querySelector("header a > img").style.height = "20px";
-        document.querySelector("header a > img").style.top = "10px";
+        document.querySelector("header a > img").style.height = "5vh";
+        document.querySelector("header a > img").src = "static/FTlogo2_landscape.png";
+
     }
     else {
-        document.getElementsByTagName("header")[0].style.padding = "12px 0px";
-        document.querySelector("header a > img").style.height = "50px";
-        document.querySelector("header a > img").style.top = "5px";
+        document.querySelector("header a > img").src = "static/FTlogo2.png";
+        document.querySelector("header a > img").style.height = "18vh";
+        document.querySelector("header a > img").style.top = "0px";
     }
 }
+
+// reveal effect
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
