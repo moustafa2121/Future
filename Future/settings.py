0d9326a3f775d44e9313f2b516d6f0ab790182ff
moustafa2121 <moustafa2121@gmail.com>
2024-01-16 02:38:57 +0400

    updated settings

--- Future/settings.py
+++ Future/settings.py
@@ -1,20 +1,9 @@
-
 import os
 import posixpath
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6210de31-ac31-4e1f-a62c-8e616b11cb62'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
@@ -100,3 +89,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+
+SECRET_KEY = '=9#$j&-7z&nm03*&8(4+$#^&&@j&jjn&oe#lh3x)$7wvis=se9'
+
+DEBUG = False
+
+# ALLOWED_HOSTS = ["futuresolutions-co.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
